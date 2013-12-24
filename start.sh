bdcaf6e46573d527bb4584412c97096403cbeeb9
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2013-12-24 11:36:06 +0100

    Checkout zotonic master branch

--- start.sh
+++ start.sh
@@ -3,9 +3,6 @@
 if [ ! -d zotonic ]; then
     # Clone the zotonic repo you are working on.
     git clone git@github.com:mmzeeman/zotonic.git
-    
-    # Checkout the branch you are working on...
-    cd zotonic; git checkout elli_machine; cd ..
 fi
 
 ## If needed, add your own-repositories here.
